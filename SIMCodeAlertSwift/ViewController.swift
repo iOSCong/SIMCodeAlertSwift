9edf577cf018349a4ff3434fb541b86eb5e5c56c
iOS <1876454300@qq.com>
2017-10-31 20:15:24 +0800

    Initial Commit

---
+++ SIMCodeAlertSwift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SIMCodeAlertSwift
+//
+//  Created by MCEJ on 2017/10/31.
+//  Copyright © 2017年 MCEJ. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
